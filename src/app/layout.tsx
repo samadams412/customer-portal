d7e05b694c80a8f57e28100a82f692756c2d1796
Shyana Sandhu <shyanas2003@gmail.com>
2025-06-23 16:59:10 -0600

    Fixed hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         {/* Wrap your Navbar and page content with the Providers component.
             This ensures that any client components (like Navbar) have access
